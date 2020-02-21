a0931e2c96aba3253d591d75177e150e9dee9763
Freddy Suarez <freddy.armando.suarez@gmail.com>
2020-02-21 15:43:24 -0300

    Updates

---
+++ precache-manifest.acac058a329ee2a59a82e5a704769ce9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e69350d0606141853278ec4daa8c01",
+    "url": "/my-app1/index.html"
+  },
+  {
+    "revision": "e0996f8522d851f178b5",
+    "url": "/my-app1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5db7273643f4751a5826",
+    "url": "/my-app1/static/js/2.dc883801.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app1/static/js/2.dc883801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0996f8522d851f178b5",
+    "url": "/my-app1/static/js/main.2a71e2fb.chunk.js"
+  },
+  {
+    "revision": "f51f3d32ccce0742979b",
+    "url": "/my-app1/static/js/runtime-main.0afae9cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app1/static/media/logo.5d5d9eef.svg"
+  }
+]);
